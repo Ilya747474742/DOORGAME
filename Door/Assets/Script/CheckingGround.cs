76ab11e04f91d296e6782be10053f2c440eb071f
Negan-lox <dmitrij.abramov.2003@mail.ru>
2021-02-18 13:22:35 +0700

    тт

---
+++ Door/Assets/Script/CheckingGround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckingGround : MonoBehaviour
+{
+   void OnTriggerExit2d(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            gameObject.GetComponentInParent<CharController>().groundCheck = false;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            gameObject.GetComponentInParent<CharController>().groundCheck = true;
+    }
+}
